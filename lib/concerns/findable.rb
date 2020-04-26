189af16519a15629b246deb5244916798b5a0576
Allen Pottinger <allenpottinger@gmail.com>
2020-04-25 21:51:35 -0400

    creates methods in concerns

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,43 @@
+module Concerns
+
+    module Savable
+  
+        def initialize(title)
+          self.title = title
+        end
+    
+        def save
+          self.class.all << self
+        end
+    
+      end
+    
+
+    module Findable
+  
+      def find_by_title(title)
+        self.all.find{|instance| instance.title == title}
+      end
+      def create(title)
+        new_instance = self.new(title)
+        new_instance.save
+        new_instance
+      end
+  
+      def find_or_create_by_title(title)
+        self.find_by_title(title) 
+        else
+        self.create(title)
+      end
+  
+      def destroy_all
+        self.all.clear
+      end
+  
+  
+    end
+  
+  
+  
+  
+  end
